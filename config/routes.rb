9bdc53af463f9fe26b746d82c9da085f89e2c78e
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-06 17:45:42 +0300

    Add style

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
   # root "foods#index"
-  resources :foods, only: [:index, :show, :new, :create, :destroy]
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
     root to: 'devise/sessions#new'
   end
   resources :users, only: [:index, :show]
-  resources :public_recipes, only: [:index]
-  resources :recipes, only: [:index, :show, :new, :create, :update, :destroy]
+
+
   resources :recipes, only: [:index, :show, :new, :create, :destroy] do
    resources :recipe_foods, only: [:new, :create, :edit, :update, :destroy]
   end
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
+  resources :public_recipes, only: [:index]
   resources :shopping_list, only: [:index]
 end
