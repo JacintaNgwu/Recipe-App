74a087c7d8819976b0c5b81782b3cb689e6a51ed
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-06 13:18:53 +0300

    Update recipe details

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_152842) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_05_155556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_152842) do
     t.time "preparation_time"
     t.time "cooking_time"
     t.string "description"
-    t.boolean "public"
+    t.boolean "public", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
