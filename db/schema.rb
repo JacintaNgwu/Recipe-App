d961195962c254f0fd2d464f88765f02ed1dd692
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-06 13:40:55 +0300

    Update db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_05_130138) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_05_155556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,8 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_130138) do
     t.string "measurement_unit"
     t.integer "unit_price"
     t.integer "quantity"
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_foods_on_user_id"
+    t.integer "user_id"
   end
 
   create_table "recipe_foods", force: :cascade do |t|
@@ -60,7 +59,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_130138) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "foods", "users"
   add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
